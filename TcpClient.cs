a569a877b5e57c67f9ec3a16d0c76ae60cfbcd85
Keunjae Jeong <38673566+TPTplayer@users.noreply.github.com>
2023-10-26 17:23:57 +0900

    initial commit

---
+++ TcpClient.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace LocalServer {
+    public partial class TcpServer {
+        public class InternalTcpClient {
+            public event MessageHandler MessageEvent;
+            public event EventHandler_PacketReceived PacketReceivedEvent;
+
+            private Socket _socket;
+            private string _ip;
+            private int _port;
+
+            private const int BUFFER_SIZE = 4096;
+
+            public InternalTcpClient(Socket client) {
+                try {
+                    _socket = client;
+
+                    var clientIP = (IPEndPoint)_socket.RemoteEndPoint;
+                    _ip = clientIP.Address.ToString();
+                    _port = clientIP.Port;
+
+                    var socketArgs = new SocketAsyncEventArgs();
+                    socketArgs.SetBuffer(new byte[BUFFER_SIZE], 0, BUFFER_SIZE);
+                    socketArgs.UserToken = _socket;
+                    socketArgs.Completed += new EventHandler<SocketAsyncEventArgs>(PacketReceived);
+                    _socket.ReceiveAsync(socketArgs);
+                }
+                catch(Exception ex) {
+                    MessageEvent(this, "EXCEPTION", ex.Message);
+                }
+            }
+
+            public string IP {
+                get { return _ip; }
+            }
+
+            public int Port {
+                get { return _port; }
+            }
+
+            public void Close() {
+                if(_socket != null && _socket.Connected) { 
+                    _socket.Shutdown(SocketShutdown.Both);
+                    _socket.Close();
+                }
+            }
+
+            private void PacketReceived(object sender, SocketAsyncEventArgs e) {
+                Socket socket;
+                byte[] packet;
+
+                try {
+                    if(_socket.Connected && e.BytesTransferred > 0) {
+                        packet = new byte[e.BytesTransferred];
+                        Buffer.BlockCopy(e.Buffer, 0, packet, 0, e.BytesTransferred);
+
+                        PacketReceivedEvent(this, packet);
+                        socket = (Socket)sender;
+                        socket.ReceiveAsync(e);
+                    }
+                }
+                catch(Exception ex) {
+                    MessageEvent(this, "EXCEPTION", ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ TcpServer.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+
+namespace LocalServer {
+    public partial class TcpServer {
+        public delegate void MessageHandler(object sender, string type, string Message);
+        public event MessageHandler MessageEvent;
+
+        public delegate void EventHandler_ClientAccepted(object sender, object obj);
+        public event EventHandler_ClientAccepted ClientAcceptedEvent;
+
+        public delegate void EventHandler_PacketReceived(object sender, byte[] packet);
+        public event EventHandler_PacketReceived PacketReceivedEvent;
+    
+        private Socket _socket;
+        private int _port;
+
+        private List<InternalTcpClient> _connectedClientList;
+
+        public TcpServer(int port) {
+            try {
+                _port = port;        
+                _connectedClientList = new List<InternalTcpClient>();
+            }
+            catch(Exception ex) {
+                MessageEvent(this, "EXCEPTION", ex.Message);
+            }
+        }
+
+        public bool Open() {
+            try {
+                var endPoint = new IPEndPoint(IPAddress.Any, _port);
+                var socketArgs = new SocketAsyncEventArgs();
+                
+                _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socketArgs.Completed += new EventHandler<SocketAsyncEventArgs>(SocketAccepted);
+                
+                _socket.Bind(endPoint);
+                _socket.Listen(50);
+                _socket.AcceptAsync(socketArgs);
+
+                MessageEvent(this, "MESSAGE", IPAddress.Any + ": server open");
+            }
+            catch(Exception ex ) {
+                MessageEvent(this, "EXCEPTION", ex.Message);
+                return false;
+            }
+            return true;
+        }
+
+        public void Close() {
+            try {
+                foreach (var client in _connectedClientList) { 
+                    client.Close();
+                }
+                _socket.Close();
+            }
+            catch(Exception ex) {
+                MessageEvent(this, "EXCEPTION", ex.Message);
+            }
+        }
+
+        private void SocketAccepted(object sender, SocketAsyncEventArgs e) {
+            InternalTcpClient client;
+            
+            try {
+                Socket clientSocket = e.AcceptSocket;
+                IPEndPoint clientEndpoint = (IPEndPoint)clientSocket.RemoteEndPoint;
+                MessageEvent(this, "MESSAGE", "accepted: " + clientEndpoint.Address.ToString());
+
+                if (ClientAcceptedEvent != null) ClientAcceptedEvent(sender, e);
+                if (clientSocket.Connected) {
+                    client = new InternalTcpClient(clientSocket);
+                    _connectedClientList.Add(client);
+                    MessageEvent(this, "MESSAGE", "connected: " + clientEndpoint.Address.ToString());
+                }
+
+                e.AcceptSocket = null;
+                _socket.AcceptAsync(e);
+            }
+            catch(Exception ex) {
+                MessageEvent(this, "EXCEPTION", ex.Message);
+            }
+        }
+
+
+    }
+}
